c8de6b495b6e059dac1268419a259b81286951ec
warriorsupergirl <nityakhurana8@gmail.com>
2024-09-28 15:42:38 -0500

    Main menu buttons work now

--- main_menu.gd
+++ main_menu.gd
@@ -9,3 +9,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://BG.tscn") #TODO: replace with whichever scene we need to start on???
+
+
+func _on_options_button_pressed() -> void:
+	$PanelContainer/OptionsButton.hide()
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit
