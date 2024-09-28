96dcaa4b28489a940f94b40b4d2ce30255757d90
Anisha-Me <anisha.menezes@gmail.com>
2024-09-28 14:28:14 -0500

    Movement?

--- player.gd
+++ player.gd
@@ -2,3 +2,16 @@ extends CharacterBody2D
 
 var speed = 50
 var motion = Vector2.ZERO
+
+func _physics_process(delta: float):
+	if Input.is_action_pressed("Down"):
+		motion.y = -speed
+	elif Input.is_action_pressed("Up"):
+		motion.y = speed
+	elif Input.is_action_pressed("Right"):
+		motion.x = speed
+	elif Input.is_action_pressed("Left"):
+		motion.x = -speed
+		
+	
+	
