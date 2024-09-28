4d27c4cf8b5e2a0073390c41bc829b985fc6be12
warriorsupergirl <nityakhurana8@gmail.com>
2024-09-28 17:50:05 -0500

    Created market

--- main_menu.gd
+++ main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://BG.tscn") #TODO: replace with whichever scene we need to start on???
+	get_tree().change_scene_to_file("res://world.tscn") #TODO: replace with whichever scene we need to start on???
 
 
 func _on_options_button_pressed() -> void:
