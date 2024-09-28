f09504db7d3332c85ea24f6f544cb48c9a4144df
Anisha-Me <anisha.menezes@gmail.com>
2024-09-28 14:59:33 -0500

    Dog is walking and idling!

--- player.gd
+++ player.gd
@@ -21,25 +21,20 @@ func _process(delta):
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
+		$AnimatedSprite2D.play("Walk")
 	else:
-		$AnimatedSprite2D.stop()
-		
-	if velocity.length() > 0:
-		motion = motion.normalized() * speed
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.animation = "Idle"
+		$AnimatedSprite2D.play("Idle")
 
 		
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
-	if velocity.x != 0 or velocity.y != 0:
+	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "Walk"
 		$AnimatedSprite2D.flip_v = false
 		# See the note below about the following boolean assignment.
 		$AnimatedSprite2D.flip_h = velocity.x < 0
+
 		
 		
 	
