d5d5b16e73e1faff321bf763ed829fcfec62db73
warriorsupergirl <nityakhurana8@gmail.com>
2024-09-28 20:16:57 -0500

    rigid bodyyyy

--- player.gd
+++ player.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends CharacterBody2D
 
 var screen_size 
 var speed = 100
@@ -60,3 +60,6 @@ func area_entered():
 
 func _on_pepper_box_area_entered(area: Area2D) -> void:
 	print("working again??")
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
