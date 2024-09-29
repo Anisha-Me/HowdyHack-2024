1b4afde669e5880e1caa39bea86e13563b7f5841
Anisha-Me <anisha.menezes@gmail.com>
2024-09-28 20:31:01 -0500

    push

--- Farm.gd
+++ Farm.gd
@@ -1,20 +1,13 @@
-extends StaticBody2D
+extends Node2D
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	print("plz")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.has_method("player_shop_method"):
-		print("yuhh market time!!!")
-		
-	# maybe this also works???:
-	# if $Market.overlaps_body($player):
-		#print()
---
+++ player.gd
@@ -0,0 +1,62 @@
+extends Area2D
+
+var screen_size 
+var speed = 100
+var motion = Vector2.ZERO
+
+signal PickUp
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("Right"):
+		velocity.x += 1
+	if Input.is_action_pressed("Left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("Down"):
+		velocity.y += 1
+	if Input.is_action_pressed("Up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play("Walk")
+	else:
+		$AnimatedSprite2D.play("Idle")
+
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "Walk"
+		$AnimatedSprite2D.flip_v = false
+		# See the note below about the following boolean assignment.
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	
+	
+func _body_entered():
+	print("helllo")
+	PickUp.emit
+
+func player_shop_method():
+	pass
+		
+		
+	
+	
+
+
+func _on_pepper_box_pick_up(vegetable: Variant) -> void:
+	print("PLEASE I BEG")
+
+
+func area_entered():
+	print("help me")
+
+
+func _on_pepper_box_area_entered(area: Area2D) -> void:
+	print("working again??")
