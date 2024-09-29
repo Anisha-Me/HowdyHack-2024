34fa8f26150a15a67ee66ab9a5352f59b1cdbcc7
warriorsupergirl <nityakhurana8@gmail.com>
2024-09-28 20:30:34 -0500

    um

---
+++ pepper_box.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+signal PickUp(vegetable)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	set_process(false)
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+	
+
+func body_entered(body):
+	print("helloooooo")
+	set_process(true)
+	
+	
+func body_exited(body: Node):
+	set_process(false)
+
+	pass # Replace with function body.
+
+
+
+
+func _on_player_area_entered(area: Area2D) -> void:
+	print("work this time?")
