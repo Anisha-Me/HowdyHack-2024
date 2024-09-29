e58e1c0b7b7a66adcb67654d1334878ec11f230e
Anisha-Me <anisha.menezes@gmail.com>
2024-09-28 22:36:46 -0500

    tomato and growing tomato

--- player.gd
+++ player.gd
@@ -24,39 +24,33 @@ func _process(delta):
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play("Walk")
+		if(vegetable == "Tomato"):
+			$AnimatedSprite2D.play("Tomato_Walk")
+		else:
+			$AnimatedSprite2D.play("Walk")
 	else:
-		$AnimatedSprite2D.play("Idle")
+		if(vegetable=="Tomato"):
+			$AnimatedSprite2D.play("Tomato_Idle")
+		else:
+			$AnimatedSprite2D.play("Idle")
 
 		
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
 	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "Walk"
+		if(vegetable == "Tomato"):
+			$AnimatedSprite2D.animation = "Tomato_Walk"
+		else:
+			$AnimatedSprite2D.animation = "Walk"
 		$AnimatedSprite2D.flip_v = false
 		# See the note below about the following boolean assignment.
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	
-	
-func _body_entered():
-	print("helllo")
-	PickUp.emit
+
 
 func player_shop_method():
 	pass
-		
-		
-	
-	
-
-
-func _on_pepper_box_pick_up(vegetable: Variant) -> void:
-	print("PLEASE I BEG")
-
-
-func area_entered():
-	print("help me")
 
 
 func _on_pepper_box_area_entered(area: Area2D) -> void:
