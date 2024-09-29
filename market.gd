dca83a65918ab5f74de02e9fc41f2ebae5d0a299
warriorsupergirl <nityakhurana8@gmail.com>
2024-09-29 01:03:59 -0500

    more market lol

--- market.gd
+++ market.gd
@@ -14,10 +14,8 @@ func _process(delta: float) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.has_method("player_shop_method"):
 		print("yuhh market time!!!")
-		
-		
-	
-		
-		
-	
-	
+		$MarketMenu.show()
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	$MarketMenu.hide()
+	pass # Replace with function body.
--- market_menu.gd
+++ market_menu.gd
@@ -3,6 +3,10 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	self.hide()
+	$leftbutton.hide()
+	$rightbutton.hide()
+	
 	pass # Replace with function body.
 
 
@@ -12,8 +16,12 @@ func _process(delta: float) -> void:
 
 
 func _on_sell_button_pressed() -> void:
-	World.money = 1
-	
+	World.money += World.num_peppers + World.num_corn + World.num_potatos + World.num_carrots + World.num_tomatoes
+	World.num_peppers = 0
+	World.num_corn = 0
+	World.num_potatos = 0
+	World.num_carrots = 0
+	World.num_tomatoes = 0
 	
 	pass # Replace with function body.
 
